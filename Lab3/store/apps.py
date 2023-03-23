0a692f94441be7648c1c3077a13bdb3213d427f1
MisanthropicSnail <119806277+MisanthropicSnail@users.noreply.github.com>
2023-03-23 09:52:39 +0300

    updated backed to corr with final ver

--- Lab3/store/apps.py
+++ Lab3/store/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class TheappConfig(AppConfig):
+class StoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'TheApp'
+    name = 'store'
